a8b8ce6174219c1b388bdd2e6e84dcfd8d7f5443
DiegoxK <diego.stv@outlook.es>
2025-05-14 10:13:47 -0500

    add initial schema definition for Post using Zod and MongoDB ObjectId

---
+++ src/server/db/schema.ts
@@ -0,0 +1,9 @@
+import { ObjectId } from "mongodb";
+import { z } from "zod";
+
+// export const PostSchema = z.object({
+//   _id: z.instanceof(ObjectId).optional(),
+//   name: z.string(),
+//   createdAt: z.date(),
+//   userId: z.string(),
+// });
