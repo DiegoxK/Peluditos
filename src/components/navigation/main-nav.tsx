1934f6ad0ac2d925f98895cf113e32a9d13a768b
DiegoxK <diego.stv@outlook.es>
2025-05-14 12:42:24 -0500

    add MainNav component for navigation

---
+++ src/components/navigation/main-nav.tsx
@@ -0,0 +1,22 @@
+import { navigation } from "@/config";
+import { Button } from "../ui/button";
+import { NavLink } from "./nav-link";
+import Link from "next/link";
+
+export default function MainNav() {
+  return (
+    <nav className="hidden items-center gap-6 text-sm md:flex">
+      <div className="flex gap-6">
+        {navigation.map((link) => (
+          <NavLink key={link.href} href={link.href}>
+            {link.label}
+          </NavLink>
+        ))}
+      </div>
+      {/* Call to action button */}
+      <Button asChild className="bg-primary px-6">
+        <Link href="/join">Adopta!</Link>
+      </Button>
+    </nav>
+  );
+}
