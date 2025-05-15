75c960e12dfefaaac8e50b880fe3581032ee8325
DiegoxK <diego.stv@outlook.es>
2025-05-15 09:55:18 -0500

    fix: adjust margins for service, news, and team sections for improved layout consistency

--- src/app/(routes)/(static)/(index)/page.tsx
+++ src/app/(routes)/(static)/(index)/page.tsx
@@ -21,14 +21,17 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <h2 className="bg-primary w-full py-8 text-center text-3xl font-medium text-white">
+      <h2 className="bg-primary mt-2 w-full py-8 text-center text-3xl font-medium text-white">
         NUESTROS SERVICIOS!
       </h2>
       <Services />
-      <h2 className="bg-primary w-full py-8 text-center text-3xl font-medium text-white">
+      <h2 className="bg-primary mb-2 w-full py-8 text-center text-3xl font-medium text-white">
         NOTICIAS!
       </h2>
       <News />
+      <h2 className="bg-primary mt-2 w-full py-8 text-center text-3xl font-medium text-white">
+        EQUIPO!
+      </h2>
       <Team />
     </main>
   );
