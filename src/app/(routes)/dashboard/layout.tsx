1b39581e8536fd173389fdfa9c6c2e96ea378e0b
DiegoxK <diego.stv@outlook.es>
2025-05-17 12:28:48 -0500

    Wrap AppSidebar and main content in DialogProvider for improved context management

--- src/app/(routes)/dashboard/layout.tsx
+++ src/app/(routes)/dashboard/layout.tsx
@@ -1,15 +1,18 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/app/(routes)/dashboard/_components/app-sidebar";
 import Header from "./_components/header";
+import { DialogProvider } from "../../../context/dialog-provider";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <Header />
-        {children}
-      </main>
+      <DialogProvider>
+        <AppSidebar />
+        <main className="w-full">
+          <Header />
+          {children}
+        </main>
+      </DialogProvider>
     </SidebarProvider>
   );
 }
