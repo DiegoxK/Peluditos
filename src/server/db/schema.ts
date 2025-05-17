037cc797fd3eb1b7e03b7fcbbb90e725ae8df52b
DiegoxK <diego.stv@outlook.es>
2025-05-17 12:26:20 -0500

    Add PetSchema and Pet type for pet data validation

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,3 +8,21 @@ export const UserSchema = z.object({
   image: z.string().optional(),
   emailVerified: z.date().optional(),
 });
+
+export const PetSchema = z.object({
+  _id: z.instanceof(ObjectId).optional(),
+  name: z.string(),
+  specie: z.string(),
+  breed: z.string(),
+  age: z.number(),
+  status: z.enum(["adoptado", "disponible"]),
+  image: z.string(),
+  entryDate: z.string(),
+  description: z.string(),
+  gender: z.enum(["Macho", "Hembra"]),
+  weight: z.number(),
+  vaccinated: z.boolean(),
+  sterilized: z.boolean(),
+});
+
+export type Pet = z.infer<typeof PetSchema>;
