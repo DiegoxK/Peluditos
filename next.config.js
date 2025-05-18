836e8ea81e8d97a060d3d30060877e13408e1732
DiegoxK <diego.stv@outlook.es>
2025-05-18 09:49:01 -0500

    Add image configuration with remote patterns to Next.js config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.ufs.sh",
+      },
+    ],
+  },
+};
 
 export default config;
