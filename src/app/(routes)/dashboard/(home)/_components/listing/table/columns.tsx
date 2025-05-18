6cbc05fc96bb1f18c3dbb394ec1d6d6ef27b7add
DiegoxK <diego.stv@outlook.es>
2025-05-17 20:09:04 -0500

    Refactor columns configuration to ensure proper filtering options for each column

--- src/app/(routes)/dashboard/(home)/_components/listing/table/columns.tsx
+++ src/app/(routes)/dashboard/(home)/_components/listing/table/columns.tsx
@@ -7,33 +7,39 @@ export const columns: ColumnDef<Pet>[] = [
   {
     accessorKey: "image",
     header: "Imagen",
+    enableGlobalFilter: false,
+    enableColumnFilter: false,
   },
   {
     accessorKey: "name",
     header: "Nombre",
+    enableGlobalFilter: true,
   },
   {
     accessorKey: "specie",
     header: "Especie",
+    enableColumnFilter: true,
+    enableGlobalFilter: true,
   },
   {
     accessorKey: "breed",
     header: "Raza",
+    enableGlobalFilter: true,
   },
   {
     accessorKey: "age",
     header: "Edad",
+    enableGlobalFilter: false,
   },
   {
     accessorKey: "status",
     header: "Estado",
+    enableColumnFilter: true,
+    enableGlobalFilter: false,
   },
   {
     accessorKey: "entryDate",
     header: "Fecha Ingreso",
+    enableGlobalFilter: false,
   },
-  // {
-  //   accessorKey: "",
-  //   header: "actions",
-  // },
 ];
