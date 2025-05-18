b779abb797530d3cd7eea93175803f4d1153cc95
DiegoxK <diego.stv@outlook.es>
2025-05-18 09:49:25 -0500

    Enhance columns configuration by adding custom cell renderers for image, name, and status fields

--- src/app/(routes)/dashboard/(home)/_components/listing/table/columns.tsx
+++ src/app/(routes)/dashboard/(home)/_components/listing/table/columns.tsx
@@ -1,18 +1,40 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
 import type { Pet } from "@/server/db/schema";
 import type { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
 
 export const columns: ColumnDef<Pet>[] = [
   {
     accessorKey: "image",
     header: "Imagen",
+    cell: ({ row }) => {
+      const { image } = row.original;
+      const { name } = row.original;
+      return (
+        <div className="bg-accent ml-2 flex size-[35px] w-fit items-center overflow-hidden rounded-full">
+          <Image
+            src={
+              "https://d9kl8ewi09.ufs.sh/f/HkC5qL7fL2Jzu5CSNzBpzFNeaBDdPhftZ9iJsW5R1T8oICYU"
+            }
+            alt={name}
+            width={35}
+            height={35}
+          />
+        </div>
+      );
+    },
     enableGlobalFilter: false,
     enableColumnFilter: false,
   },
   {
     accessorKey: "name",
     header: "Nombre",
+    cell: ({ row }) => {
+      const { name } = row.original;
+      return <span className="text-primary font-semibold">{name}</span>;
+    },
     enableGlobalFilter: true,
   },
   {
@@ -34,6 +56,25 @@ export const columns: ColumnDef<Pet>[] = [
   {
     accessorKey: "status",
     header: "Estado",
+    cell: ({ row }) => {
+      const { status } = row.original;
+      return (
+        <Badge
+          className="capitalize"
+          variant={
+            status === "disponible"
+              ? "default"
+              : status === "adoptado"
+                ? "secondary"
+                : status === "en tratamiento"
+                  ? "outline"
+                  : "destructive"
+          }
+        >
+          {status}
+        </Badge>
+      );
+    },
     enableColumnFilter: true,
     enableGlobalFilter: false,
   },
