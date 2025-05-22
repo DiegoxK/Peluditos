92672d5ad568ca89636f8a0b1c426f52e23906d2
DiegoxK <diego.stv@outlook.es>
2025-05-22 12:37:59 -0500

    Refactor utils.ts to improve date formatting function and ensure consistent code style

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatDate = (dateStr: string) => {
+  if (!dateStr) return "";
+  try {
+    const date = new Date(dateStr);
+    return format(date, "dd/MM/yyyy", { locale: es });
+  } catch (error) {
+    return dateStr;
+  }
+};
