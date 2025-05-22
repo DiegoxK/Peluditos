a83f7de6d90601cf70c3b1067d1d15a696637f55
DiegoxK <diego.stv@outlook.es>
2025-05-22 10:31:17 -0500

    Implement authentication check in Layout component to redirect unauthenticated users

--- src/app/(routes)/dashboard/layout.tsx
+++ src/app/(routes)/dashboard/layout.tsx
@@ -2,8 +2,20 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/app/(routes)/dashboard/_components/app-sidebar";
 import Header from "./_components/header";
 import { DialogProvider } from "../../../context/dialog-provider";
+import { auth } from "@/server/auth";
+import { redirect } from "next/navigation";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/auth/login");
+  }
+
   return (
     <SidebarProvider>
       <DialogProvider>
