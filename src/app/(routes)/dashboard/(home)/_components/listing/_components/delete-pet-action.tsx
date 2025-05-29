0fc350a8eb540f2cea0f571d8f2e9aae65258575
DiegoxK <diego.stv@outlook.es>
2025-05-29 12:35:42 -0500

    Implement delete functionality in DeletePetAction component

--- src/app/(routes)/dashboard/(home)/_components/listing/_components/delete-pet-action.tsx
+++ src/app/(routes)/dashboard/(home)/_components/listing/_components/delete-pet-action.tsx
@@ -1,18 +1,39 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { DialogClose, DialogFooter } from "@/components/ui/dialog";
 import type { Pet } from "@/server/db/schema";
+import { api } from "@/trpc/react";
 
 interface DeletePetProps {
   pet: Pet;
 }
 
 export default function DeletePetAction({ pet }: DeletePetProps) {
+  const { mutate: deletePet } = api.pets.deletePet.useMutation();
+
+  const handleDelete = () => {
+    deletePet(
+      { _id: pet._id },
+      {
+        onSuccess: () => {
+          console.log("Pet deleted successfully");
+        },
+        onError: (error) => {
+          console.error("Error deleting pet:", error);
+        },
+      },
+    );
+  };
+
   return (
     <DialogFooter className="mt-4">
       <DialogClose asChild>
         <Button variant="outline">Cerrar</Button>
       </DialogClose>
-      <Button variant="destructive">Eliminar</Button>
+      <Button onClick={handleDelete} variant="destructive">
+        Eliminar
+      </Button>
     </DialogFooter>
   );
 }
