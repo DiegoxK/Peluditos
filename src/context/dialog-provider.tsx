2216babc1b37aac67c5ad58ec37313f86afc133c
DiegoxK <diego.stv@outlook.es>
2025-05-30 10:30:54 -0500

    Enhance DialogProvider to manage submission state and prevent modal closure

--- src/context/dialog-provider.tsx
+++ src/context/dialog-provider.tsx
@@ -18,6 +18,7 @@ type DialogOptions = {
 type DialogContextType = {
   openDialog: (options: DialogOptions) => void;
   closeDialog: () => void;
+  setSubmitting: (submitting: boolean) => void;
 };
 
 const DialogContext = createContext<DialogContextType | undefined>(undefined);
@@ -25,6 +26,12 @@ const DialogContext = createContext<DialogContextType | undefined>(undefined);
 export const DialogProvider = ({ children }: { children: ReactNode }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [dialogContent, setDialogContent] = useState<DialogOptions>({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  // Prevents user from clossing the modal
+  const setSubmitting = (submitting: boolean) => {
+    setIsSubmitting(submitting);
+  };
 
   const openDialog = (options: DialogOptions) => {
     setDialogContent(options);
@@ -41,10 +48,21 @@ export const DialogProvider = ({ children }: { children: ReactNode }) => {
   };
 
   return (
-    <DialogContext.Provider value={{ openDialog, closeDialog }}>
+    <DialogContext.Provider value={{ openDialog, closeDialog, setSubmitting }}>
       {children}
       <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <DialogContent className="max-h-[90vh]">
+        <DialogContent
+          onEscapeKeyDown={(event) => {
+            if (isSubmitting) event.preventDefault();
+          }}
+          onPointerDownOutside={(event) => {
+            if (isSubmitting) event.preventDefault();
+          }}
+          onInteractOutside={(event) => {
+            if (isSubmitting) event.preventDefault();
+          }}
+          className="max-h-[90vh]"
+        >
           <DialogHeader>
             {dialogContent.title && (
               <DialogTitle>{dialogContent.title}</DialogTitle>
