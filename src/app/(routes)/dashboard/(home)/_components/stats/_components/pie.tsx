0c23e610c38060a3541c9ce93aff1ea9fea29268
DiegoxK <diego.stv@outlook.es>
2025-06-28 10:52:13 -0500

    Refactor ChartPieSimple component to display species data and update chart colors

--- src/app/(routes)/dashboard/(home)/_components/stats/_components/pie.tsx
+++ src/app/(routes)/dashboard/(home)/_components/stats/_components/pie.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
+import { Cat, TrendingUp } from "lucide-react";
 import { Pie, PieChart } from "recharts";
 
 import {
@@ -18,69 +18,49 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+import { Separator } from "@/components/ui/separator";
 
 const chartData = [
-  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
-  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+  { specie: "perro", amount: 5, fill: "var(--chart-1)" },
+  { specie: "gato", amount: 15, fill: "var(--chart-2)" },
 ];
 
 const chartConfig = {
-  visitors: {
-    label: "Visitors",
+  perro: {
+    label: "Perro",
   },
-  chrome: {
-    label: "Chrome",
-    color: "var(--chart-1)",
-  },
-  safari: {
-    label: "Safari",
-    color: "var(--chart-2)",
-  },
-  firefox: {
-    label: "Firefox",
-    color: "var(--chart-3)",
-  },
-  edge: {
-    label: "Edge",
-    color: "var(--chart-4)",
-  },
-  other: {
-    label: "Other",
-    color: "var(--chart-5)",
+  gato: {
+    label: "Gato",
   },
 } satisfies ChartConfig;
 
 export function ChartPieSimple() {
   return (
-    <Card className="flex flex-col">
-      <CardHeader>
-        <CardTitle>Pie Chart</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
-      </CardHeader>
-      <CardContent className="flex-1 pb-0">
+    <Card>
+      <CardTitle className="text-primary flex items-center gap-2">
+        <Cat strokeWidth={2.2} />
+        <span className="font-semibold">Especies</span>
+      </CardTitle>
+
+      <div className="mx-4">
+        <Separator className="bg-input" />
+      </div>
+      <CardContent>
         <ChartContainer
           config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
+          className="aspect-square size-64 max-h-64"
         >
           <PieChart>
             <ChartTooltip
               cursor={false}
               content={<ChartTooltipContent hideLabel />}
             />
-            <Pie data={chartData} dataKey="visitors" nameKey="browser" />
+            <Pie data={chartData} dataKey="amount" nameKey="specie" />
           </PieChart>
         </ChartContainer>
       </CardContent>
       <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 leading-none font-medium">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="text-muted-foreground leading-none">
-          Showing total visitors for the last 6 months
-        </div>
+        <CardDescription>January - June 2024</CardDescription>
       </CardFooter>
     </Card>
   );
