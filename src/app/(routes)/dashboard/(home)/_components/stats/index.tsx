0eabfdfea90a82883f7494d3daabe9087730d8a2
DiegoxK <diego.stv@outlook.es>
2025-07-05 11:47:50 -0500

    Add NewPetsAreaChart to PetStats component layout

--- src/app/(routes)/dashboard/(home)/_components/stats/index.tsx
+++ src/app/(routes)/dashboard/(home)/_components/stats/index.tsx
@@ -1,13 +1,17 @@
 import { AdoptionRadial } from "./_components/adoption-radial";
+import { NewPetsAreaChart } from "./_components/new-pets-area-chart";
 import { SpeciesPie } from "./_components/species-pie";
 import { VaccineRadial } from "./_components/vaccine-radial";
 
 export default function PetStats() {
   return (
-    <div className="grid grid-cols-3 gap-6">
-      <SpeciesPie />
-      <AdoptionRadial />
-      <VaccineRadial />
-    </div>
+    <>
+      <div className="grid grid-cols-3 gap-6">
+        <SpeciesPie />
+        <AdoptionRadial />
+        <VaccineRadial />
+      </div>
+      <NewPetsAreaChart />
+    </>
   );
 }
