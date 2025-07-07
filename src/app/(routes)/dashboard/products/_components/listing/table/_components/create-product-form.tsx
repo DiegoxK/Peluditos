241e9322605461d57c543c4967ca0bc39ff82286
DiegoxK <diego.stv@outlook.es>
2025-07-07 15:05:16 -0500

    Added create product form based on the pets form

---
+++ src/app/(routes)/dashboard/products/_components/listing/table/_components/create-product-form.tsx
@@ -0,0 +1,518 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Switch } from "@/components/ui/switch";
+import { Button } from "@/components/ui/button";
+import { DialogClose, DialogFooter } from "@/components/ui/dialog";
+import type { Product } from "@/server/db/schema";
+import {
+  ImageCropApplyAction,
+  ImageCropCloseAction,
+  ImageCropChangeAction,
+  ImageCropContentArea,
+  ImageCropCropper,
+  ImageCropDescription,
+  ImageCropFooter,
+  ImageCropHeader,
+  ImageCropPreview,
+  ImageCropRoot,
+  ImageCropTitle,
+  ImageCropTrigger,
+} from "@/components/ui/image-crop-area";
+import { Package as PackageIcon, Trash2, X } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { uploadFiles } from "@/lib/uploadthing";
+import { api } from "@/trpc/react";
+import { toast } from "sonner";
+import { useRef, useState } from "react";
+import { useDialog } from "@/context/dialog-provider";
+import { useTableState } from "@/context/table-state-provider";
+import { FeatureInput } from "@/components/ui/feature-input";
+
+const ProductImageSchema = z.union([
+  z
+    .string({
+      message: "La imagen del producto es obligatoria.",
+    })
+    .url({ message: "Debe ser una URL válida." }),
+  z.instanceof(Blob, { message: "La imagen debe ser un archivo válido." }),
+]);
+
+const formSchema = z.object({
+  name: z
+    .string({ required_error: "El nombre es obligatorio." })
+    .min(1, { message: "El nombre no puede estar vacío." }),
+  category: z
+    .string({ required_error: "La categoría es obligatoria." })
+    .min(1, { message: "La categoría no puede estar vacía." }),
+  subcategory: z
+    .string({ required_error: "La subcategoría es obligatoria." })
+    .min(1, { message: "La subcategoría no puede estar vacía." }),
+  price: z.coerce
+    .number({
+      invalid_type_error: "El precio debe ser un número.",
+      required_error: "El precio es obligatorio.",
+    })
+    .positive({ message: "El precio debe ser mayor a cero." }),
+  stock: z.coerce
+    .number({
+      invalid_type_error: "El stock debe ser un número.",
+      required_error: "El stock es obligatorio.",
+    })
+    .int({ message: "El stock debe ser un número entero." })
+    .nonnegative({ message: "El stock no puede ser negativo." }),
+  minStock: z.coerce
+    .number({
+      invalid_type_error: "El stock mínimo debe ser un número.",
+      required_error: "El stock mínimo es obligatorio.",
+    })
+    .int({ message: "El stock mínimo debe ser un número entero." })
+    .nonnegative({ message: "El stock mínimo no puede ser negativo." }),
+  description: z
+    .string({ required_error: "La descripción es obligatoria." })
+    .min(10, { message: "La descripción debe tener al menos 10 caracteres." }),
+
+  features: z.array(z.string()),
+  image: ProductImageSchema,
+  featured: z.boolean(),
+});
+
+type ProductFormData = z.infer<typeof formSchema>;
+
+interface CreateProductFormProps {
+  product?: Product;
+}
+
+export default function CreateProductForm({ product }: CreateProductFormProps) {
+  const { closeDialog, setPreventDialogClose } = useDialog();
+  const { currentQueryInput, resetToFirstPage } = useTableState();
+  const [isFormSubmitting, setIsFormSubmitting] = useState(false);
+
+  const isEditMode = Boolean(product);
+  const utils = api.useUtils();
+
+  const form = useForm<ProductFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: product?.name ?? "",
+      category: product?.category ?? "",
+      subcategory: product?.subcategory ?? "",
+      price: product?.price ?? 0,
+      stock: product?.stock ?? 0,
+      minStock: product?.minStock ?? 5,
+      description: product?.description ?? "",
+      features: product?.features ?? [],
+      image: product?.image ?? "",
+      featured: product?.featured ?? false,
+    },
+  });
+
+  const { mutate: createProduct } = api.products.createProduct.useMutation({
+    onMutate: async (newProductApiInput) => {
+      await utils.products.getAllProducts.cancel(currentQueryInput);
+      const previousProductsResponse =
+        utils.products.getAllProducts.getData(currentQueryInput);
+
+      utils.products.getAllProducts.setData(
+        currentQueryInput,
+        (oldResponse) => {
+          if (!oldResponse) return undefined;
+
+          const tempId = "temp-" + Math.random().toString(36).slice(2, 9);
+          const optimisticProductEntry: Product = {
+            ...newProductApiInput,
+            _id: tempId,
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+            previousPrice: newProductApiInput.price,
+            sales: 0,
+            views: 0,
+          };
+
+          return {
+            ...oldResponse,
+            data: [optimisticProductEntry, ...oldResponse.data],
+            totalRowCount: oldResponse.totalRowCount + 1,
+          };
+        },
+      );
+      return { previousProductsResponse, queryInputUsed: currentQueryInput };
+    },
+    onSuccess: (data) => console.log("Product created successfully:", data),
+    onError: (error, _newProduct, context) => {
+      if (context?.previousProductsResponse) {
+        utils.products.getAllProducts.setData(
+          context.queryInputUsed,
+          context.previousProductsResponse,
+        );
+      }
+      toast.error(`Error creando producto: ${error.message}`, {
+        id: "product-form",
+      });
+    },
+    onSettled: (_data, error) => {
+      void utils.products.getAllProducts.invalidate();
+      void utils.products.getDashboardSummary.invalidate();
+      setPreventDialogClose(false);
+
+      if (!error) {
+        toast.success("Producto creado exitosamente!", { id: "product-form" });
+        resetToFirstPage();
+        closeDialog();
+      }
+    },
+  });
+
+  const { mutate: updateProduct } = api.products.updateProduct.useMutation({
+    onMutate: async (updatedProductPayload) => {
+      await utils.products.getAllProducts.cancel(currentQueryInput);
+      const previousProductsResponse =
+        utils.products.getAllProducts.getData(currentQueryInput);
+
+      utils.products.getAllProducts.setData(
+        currentQueryInput,
+        (oldResponse) => {
+          if (!oldResponse) return undefined;
+          return {
+            ...oldResponse,
+            data: oldResponse.data.map((p) =>
+              p._id === updatedProductPayload._id
+                ? {
+                    ...p,
+                    ...updatedProductPayload,
+                    updatedAt: new Date().toISOString(),
+                  }
+                : p,
+            ),
+          };
+        },
+      );
+      return { previousProductsResponse, queryInputUsed: currentQueryInput };
+    },
+    onError: (error, _updatedPet, context) => {
+      if (context?.previousProductsResponse) {
+        utils.products.getAllProducts.setData(
+          context.queryInputUsed,
+          context.previousProductsResponse,
+        );
+      }
+      toast.error(`Error actualizando producto: ${error.message}`);
+    },
+    onSuccess: (data) => console.log("Product updated successfully:", data),
+    onSettled: (_data, error) => {
+      void utils.products.getAllProducts.invalidate();
+      void utils.products.getDashboardSummary.invalidate();
+
+      if (!error) {
+        toast.success("Producto actualizado exitosamente!");
+      }
+      resetToFirstPage();
+      setIsFormSubmitting(false);
+      if (!error) closeDialog();
+    },
+  });
+
+  async function onSubmit(values: ProductFormData) {
+    setIsFormSubmitting(true);
+    if (isEditMode) {
+      toast.loading("Actualizando producto...", { id: "product-form" });
+    } else {
+      toast.loading("Creando producto...", { id: "product-form" });
+      setPreventDialogClose(true);
+    }
+
+    const productImage = values.image;
+    let finalImageUrl: string | null = null;
+    let imageKey: string | null = null;
+
+    if (productImage instanceof Blob) {
+      try {
+        const file = new File(
+          [productImage],
+          `${values.name.toLowerCase().replace(/\s+/g, "-")}.webp`,
+          {
+            type: "image/webp",
+          },
+        );
+        const res = await uploadFiles("imageUploader", { files: [file] });
+        const uploadedFile = res?.[0];
+
+        if (!uploadedFile?.ufsUrl || !uploadedFile.key)
+          throw new Error("Fallo en la subida de imagen.");
+
+        finalImageUrl = uploadedFile.ufsUrl;
+        imageKey = uploadedFile.key;
+        toast.success("Imagen subida con éxito!", { id: "image-upload" });
+      } catch (error) {
+        toast.error("Error al subir la imagen.", { id: "image-upload" });
+        setIsFormSubmitting(false);
+        setPreventDialogClose(false);
+        return;
+      }
+    } else if (typeof productImage === "string" && productImage.trim()) {
+      finalImageUrl = productImage;
+      imageKey = product?.imageKey ?? "";
+    } else {
+      toast.error("La imagen es obligatoria.");
+      setIsFormSubmitting(false);
+      setPreventDialogClose(false);
+      return;
+    }
+
+    const payload = {
+      ...values,
+      image: finalImageUrl,
+      imageKey: imageKey,
+    };
+
+    if (isEditMode && product?._id) {
+      void updateProduct({
+        _id: product._id,
+        ...payload,
+        previousPrice: product.previousPrice,
+        sales: product.sales,
+        views: product.views,
+      });
+    } else {
+      void createProduct(payload);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="max-h-[65vh] space-y-6 overflow-y-scroll py-4 ps-1 pr-3">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre del Producto</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Ej: Collar de cuero para perros"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex gap-4">
+            <FormField
+              control={form.control}
+              name="category"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Categoría</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Ej: Accesorios" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="subcategory"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Subcategoría</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Ej: Collares" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex gap-4">
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Precio</FormLabel>
+                  <FormControl>
+                    <Input type="number" step="10" min={0} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="stock"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Stock disponible</FormLabel>
+                  <FormControl>
+                    <Input type="number" min={0} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="minStock"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Stock Mínimo</FormLabel>
+                  <FormControl>
+                    <Input type="number" min={0} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Imagen del Producto</FormLabel>
+                <FormControl>
+                  <ImageCropRoot
+                    value={field.value}
+                    onChange={field.onChange}
+                    aspectRatio={1 / 1}
+                    maxFileSizeMB={4}
+                    outputOptions={{
+                      outputType: "image/webp",
+                      outputQuality: 0.7,
+                    }}
+                  >
+                    <div className="space-y-4">
+                      <ImageCropTrigger className="w-auto" />
+                      <ImageCropContentArea className="bg-card mx-auto w-full rounded-lg border p-4 shadow-sm sm:max-w-lg">
+                        <ImageCropHeader>
+                          <ImageCropTitle>Ajustar Imagen</ImageCropTitle>
+                          <ImageCropCloseAction size="icon" variant="ghost" />
+                        </ImageCropHeader>
+                        <Separator className="my-4" />
+                        <ImageCropCropper />
+                        <Separator className="my-4" />
+                        <ImageCropFooter>
+                          <ImageCropChangeAction>Cambiar</ImageCropChangeAction>
+                          <ImageCropApplyAction>Aplicar</ImageCropApplyAction>
+                        </ImageCropFooter>
+                      </ImageCropContentArea>
+                      <ImageCropPreview
+                        className="my-8 h-32 w-32 place-self-center rounded-lg border"
+                        placeholder={
+                          <div className="flex flex-col items-center justify-center gap-1">
+                            <PackageIcon strokeWidth={1.2} size={48} />
+                            <span>Producto</span>
+                          </div>
+                        }
+                      />
+                    </div>
+                  </ImageCropRoot>
+                </FormControl>
+                <FormDescription>
+                  Selecciona y recorta la imagen principal del producto.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Descripción</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Describe el producto, sus materiales, uso, etc."
+                    {...field}
+                    rows={5}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div>
+            <FormField
+              control={form.control}
+              name="features"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Features</FormLabel>
+                  <FormControl>
+                    <FeatureInput
+                      placeholder="Ingrese las caracteristicas del producto..."
+                      value={field.value}
+                      onChange={field.onChange}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name="featured"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                <div className="space-y-0.5">
+                  <FormLabel className="text-sm font-medium">
+                    Producto Destacado
+                  </FormLabel>
+                  <FormDescription>
+                    Si se activa, aparecerá en la sección principal de la
+                    tienda.
+                  </FormDescription>
+                </div>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <DialogFooter className="mt-6">
+          <Button
+            disabled={
+              (!form.formState.isDirty && isEditMode) || isFormSubmitting
+            }
+            type="submit"
+          >
+            {isEditMode ? "Actualizar Producto" : "Guardar Producto"}
+          </Button>
+          <DialogClose asChild>
+            <Button disabled={isFormSubmitting} variant="outline">
+              Cerrar
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </form>
+    </Form>
+  );
+}
