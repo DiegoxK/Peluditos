0bf9fae91855c1f6b75b50c993948b1205045f99
DiegoxK <diego.stv@outlook.es>
2025-07-07 14:51:02 -0500

    added stats tab to products

---
+++ src/app/(routes)/dashboard/products/_components/stats/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { api } from "@/trpc/react";
+
+export default function PetStats() {
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-6">stats</div>
+    </>
+  );
+}
