b1855ee5a5f6dc130017fe7c23362e1727eb17cf
DiegoxK <diego.stv@outlook.es>
2025-07-08 12:47:51 -0500

    updated command styles

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -147,7 +147,7 @@ function CommandItem({
     <CommandPrimitive.Item
       data-slot="command-item"
       className={cn(
-        "data-[selected=true]:bg-primary data-[selected=true]:text-primary-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "data-[selected=true]:bg-primary [&_svg]:text-muted-foreground relative flex cursor-pointer items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 data-[selected=true]:text-white [&_svg]:pointer-events-none [&_svg]:shrink-0 data-[selected=true]:[&_svg]:text-white [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
       {...props}
