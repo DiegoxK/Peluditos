2e6f3dc4e2bfd7f30512fac58aa1c4d410afa29a
DiegoxK <diego.stv@outlook.es>
2025-07-25 12:26:18 -0500

    changed hardcoded values for the current db data

--- src/app/(routes)/dashboard/products/_components/stats/index.tsx
+++ src/app/(routes)/dashboard/products/_components/stats/index.tsx
@@ -4,25 +4,58 @@ import { api } from "@/trpc/react";
 import { CategoriesPie } from "./_components/categories-pie";
 import { ConversionRate } from "./_components/conversion-rate";
 import { ProductsOnDiscount } from "./_components/products-on-discount";
+import { NewProductsAreaChart } from "./_components/new-products";
 
 export default function ProductStats() {
-  const productStats = {
-    totalProducts: 50,
-    totalDiscounts: 10,
-    totalCategories: 5,
-    categoryData: [
-      { id: "1", name: "Electronics", amount: 120 },
-      { id: "2", name: "Clothing", amount: 80 },
-      { id: "3", name: "Home Appliances", amount: 60 },
-      { id: "4", name: "Books", amount: 40 },
-      { id: "5", name: "Toys", amount: 30 },
-    ],
-    topProduct: {
-      name: "Papaya criolla",
-      sales: 200,
-      views: 300,
-    },
-  };
+  const [productStats] = api.products.getStats.useSuspenseQuery();
+
+  // const productStats = {
+  //   totalProducts: 50,
+  //   totalDiscounts: 10,
+  //   totalCategories: 5,
+  //   categoryData: [
+  //     { id: "1", name: "Electronics", amount: 120 },
+  //     { id: "2", name: "Clothing", amount: 80 },
+  //     { id: "3", name: "Home Appliances", amount: 60 },
+  //     { id: "4", name: "Books", amount: 40 },
+  //     { id: "5", name: "Toys", amount: 30 },
+  //   ],
+  //   topProduct: {
+  //     name: "Papaya criolla",
+  //     sales: 200,
+  //     views: 300,
+  //   },
+  //   registry: {
+  //     "2025": [
+  //       { month: "January", products: 5 },
+  //       { month: "February", products: 8 },
+  //       { month: "March", products: 12 },
+  //       { month: "April", products: 10 },
+  //       { month: "May", products: 15 },
+  //       { month: "June", products: 20 },
+  //       { month: "July", products: 18 },
+  //       { month: "August", products: 22 },
+  //       { month: "September", products: 25 },
+  //       { month: "October", products: 30 },
+  //       { month: "November", products: 28 },
+  //       { month: "December", products: 35 },
+  //     ],
+  //     "2024": [
+  //       { month: "January", products: 3 },
+  //       { month: "February", products: 6 },
+  //       { month: "March", products: 9 },
+  //       { month: "April", products: 7 },
+  //       { month: "May", products: 11 },
+  //       { month: "June", products: 14 },
+  //       { month: "July", products: 16 },
+  //       { month: "August", products: 19 },
+  //       { month: "September", products: 21 },
+  //       { month: "October", products: 24 },
+  //       { month: "November", products: 26 },
+  //       { month: "December", products: 29 },
+  //     ],
+  //   },
+  // };
 
   return (
     <>
@@ -37,6 +70,7 @@ export default function ProductStats() {
           discounts={productStats.totalDiscounts}
         />
       </div>
+      <NewProductsAreaChart registry={productStats.registry} />
     </>
   );
 }
