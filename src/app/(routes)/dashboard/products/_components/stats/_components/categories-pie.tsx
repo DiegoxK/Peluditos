6d4549af121aa28eeee528df5527c8222d2d2821
DiegoxK <diego.stv@outlook.es>
2025-07-25 11:38:24 -0500

    updated pie description

--- src/app/(routes)/dashboard/products/_components/stats/_components/categories-pie.tsx
+++ src/app/(routes)/dashboard/products/_components/stats/_components/categories-pie.tsx
@@ -123,7 +123,7 @@ export function CategoriesPie({ categories, total }: CategoriesPieProps) {
         <Separator className="bg-border" />
       </div>
       <CardFooter className="flex-col gap-2 text-sm">
-        <CardDescription>Distribucion Top 5 categorias</CardDescription>
+        <CardDescription>Distribucion por categorias</CardDescription>
       </CardFooter>
     </Card>
   );
