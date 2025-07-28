8df97479826aa7a1ab000cfd62cbfd97c0625720
DiegoxK <diego.stv@outlook.es>
2025-07-28 09:52:26 -0500

    implement transactions page with tabs and data fetching

--- src/app/(routes)/dashboard/transactions/page.tsx
+++ src/app/(routes)/dashboard/transactions/page.tsx
@@ -1,3 +1,53 @@
-export default function TransactionsPage() {
-  return <div>TransactionsPage</div>;
+import { api, HydrateClient } from "@/trpc/server";
+
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { TableStateProvider } from "@/context/table-state-provider";
+
+import { defaultInitialTableQueryInput } from "@/config/filter-defaults";
+import { DialogProvider } from "@/context/dialog-provider";
+
+import PetListing from "./_components/listing";
+import PetStats from "./_components/stats";
+
+export default async function TransactionsPage() {
+  void api.pets.getAllPets.prefetch(defaultInitialTableQueryInput);
+  void api.pets.getDashboardSummary.prefetch();
+  void api.pets.getStats.prefetch();
+
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-primary text-3xl font-bold tracking-tight">
+        Gestión de Pedidos
+      </h1>
+      <p className="text-muted-foreground">
+        Administra y da seguimiento a los pedidos de tus clientes
+      </p>
+
+      <div className="my-4">
+        <Separator className="bg-input" />
+      </div>
+
+      <Tabs defaultValue="listado" className="space-y-4">
+        <TabsList>
+          <TabsTrigger value="listado">Pedidos</TabsTrigger>
+          <TabsTrigger value="estadisticas">Estadísticas</TabsTrigger>
+        </TabsList>
+
+        <HydrateClient>
+          <TableStateProvider>
+            <DialogProvider>
+              <TabsContent value="listado" className="space-y-4">
+                <PetListing />
+              </TabsContent>
+              <TabsContent value="estadisticas" className="space-y-4">
+                <PetStats />
+              </TabsContent>
+            </DialogProvider>
+          </TableStateProvider>
+        </HydrateClient>
+      </Tabs>
+    </div>
+  );
 }
