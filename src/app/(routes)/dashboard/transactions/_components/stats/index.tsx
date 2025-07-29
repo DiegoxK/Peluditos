73bbc37ab01f3de63e7018322901b30e71734c44
DiegoxK <diego.stv@outlook.es>
2025-07-29 11:58:08 -0500

    add RevenueOverTime component to display revenue data over time

--- src/app/(routes)/dashboard/transactions/_components/stats/index.tsx
+++ src/app/(routes)/dashboard/transactions/_components/stats/index.tsx
@@ -4,6 +4,7 @@ import { api } from "@/trpc/react";
 import { StatusPie } from "./_components/status-pie";
 import { ShippedRadial } from "./_components/shipped-radial";
 import { CancelationRate } from "./_components/cancelation-rate";
+import { RevenueOverTime } from "./_components/revenue-over-time";
 
 export default function TransactionStats() {
   const transactionStats = {
@@ -17,6 +18,36 @@ export default function TransactionStats() {
     shipped: {
       total: 100,
       delivered: 80,
+    },
+    revenue: {
+      "2025": [
+        { month: "January", revenue: 50000 },
+        { month: "February", revenue: 888489 },
+        { month: "March", revenue: 0 },
+        { month: "April", revenue: 1050 },
+        { month: "May", revenue: 2156 },
+        { month: "June", revenue: 486489 },
+        { month: "July", revenue: 0 },
+        { month: "August", revenue: 0 },
+        { month: "September", revenue: 0 },
+        { month: "October", revenue: 0 },
+        { month: "November", revenue: 0 },
+        { month: "December", revenue: 0 },
+      ],
+      "2024": [
+        { month: "January", revenue: 50000 },
+        { month: "February", revenue: 888489 },
+        { month: "March", revenue: 0 },
+        { month: "April", revenue: 1050 },
+        { month: "May", revenue: 2156 },
+        { month: "June", revenue: 486489 },
+        { month: "July", revenue: 486489 },
+        { month: "August", revenue: 486489 },
+        { month: "September", revenue: 486489 },
+        { month: "October", revenue: 486489 },
+        { month: "November", revenue: 486489 },
+        { month: "December", revenue: 486548 },
+      ],
     },
   };
 
@@ -40,6 +71,7 @@ export default function TransactionStats() {
           }
         />
       </div>
+      <RevenueOverTime registry={transactionStats.revenue} />
     </>
   );
 }
