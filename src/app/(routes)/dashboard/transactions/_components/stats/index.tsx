05648fd214a6c12717ad142945638ebaccdc21ee
DiegoxK <diego.stv@outlook.es>
2025-07-29 11:25:06 -0500

    refactor TransactionStats component to display transaction statistics with StatusPie visualization

--- src/app/(routes)/dashboard/transactions/_components/stats/index.tsx
+++ src/app/(routes)/dashboard/transactions/_components/stats/index.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import { api } from "@/trpc/react";
+import { StatusPie } from "./_components/status-pie";
 
-export default function PetStats() {
-  const [petStats] = api.pets.getStats.useSuspenseQuery();
+export default function TransactionStats() {
+  const transactionStats = {
+    totalOrders: 110,
+    statuses: [
+      { label: "enviado", amount: 20 },
+      { label: "procesando", amount: 5 },
+      { label: "entregado", amount: 100 },
+      { label: "cancelado", amount: 2 },
+    ],
+  };
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-6">stats</div>
+      <div className="grid grid-cols-3 gap-6">
+        <StatusPie
+          statuses={transactionStats.statuses}
+          total={transactionStats.totalOrders}
+        />
+      </div>
     </>
   );
 }
