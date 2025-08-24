fc21ccfc24a94cdcfd63e6a6c2651d4778196e13
DiegoxK <diego.stv@outlook.es>
2025-08-24 11:20:10 -0500

    updated toaster component to always use light colors

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -4,7 +4,7 @@ import { useTheme } from "next-themes";
 import { Toaster as Sonner, type ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
+  const { theme = "light" } = useTheme();
 
   return (
     <Sonner
